34ae1d7738de3758febcc6a6b0ee2bd01baf5795
zdeyn <i.am.zdeyn@gmail.com>
2024-05-24 06:56:04 +1000

    chore: Add flask dependency and remove unused modules
    This commit adds the following changes:
    - Updated `requirements.txt` to include the `flask` dependency
    - Removed unused modules: `zbricks.core.zbricks`, `zbricks.core.zmachine`, `zbricks.core.bases`, `zbricks.core.types`, `zbricks.core.entities`, `tests/core/test_zevent.py`, and `zbricks.core.events.py`
    
    Note: This is a project reset. All tests pass.

---
+++ tests/test_basics.py
@@ -0,0 +1,13 @@
+# tests/test_basics.py
+import pytest
+from rich import print
+
+from zbricks import zApp, Flask, Request, Response
+
+
+def test_responds(app : zApp) -> None:
+            
+    with app.test_client() as client:
+        response = client.get('/')
+        assert isinstance(response, Response)
+        
