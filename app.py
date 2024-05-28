f17762319d3ff15a23d647d8813f374d90df1630
zdeyn <i.am.zdeyn@gmail.com>
2024-05-28 16:02:52 +1000

    chore: Update Flask imports, add index route, templating

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, get_flashed_messages, current_app
+from flask_login import current_user
 from config import DevelopmentConfig
 from zbricks.auth import zAuth
 
@@ -7,6 +8,11 @@ def create_app(config_class=DevelopmentConfig):
     app.config.from_object(config_class)
     
     zauth = zAuth(app)
+
+    @app.route('/')
+    def index():
+        flash_data = get_flashed_messages(with_categories=True, category_filter=['success'])
+        return render_template('index.html', content="Woot!")
     
     return app
 
--- zbricks/auth/__init__.py
+++ zbricks/auth/__init__.py
@@ -1,8 +1,11 @@
 import logging
 from typing import Optional
-from flask import Flask
+from flask import Flask, get_flashed_messages, url_for
+
+from jinja2 import PackageLoader, ChoiceLoader
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select
+from flask_login import current_user
 
 from .blueprint import auth_bp, save_token, update_token
 from .models import User
@@ -15,6 +18,12 @@ class zAuth:
     
     def init_app(self, app: Flask):
         # app.logger.setLevel(logging.DEBUG)
+
+        app.jinja_loader = ChoiceLoader([
+            app.jinja_loader, # type: ignore
+            PackageLoader('zbricks.auth', 'templates'),
+            PackageLoader('zbricks', 'templates')
+        ])
         
         for m in [db, jwtm, login_manager, oauth]:
             # app.logger.debug(f'Initializing {m}')
@@ -36,7 +45,7 @@ class zAuth:
             client_kwargs = {'scope': 'identify email'}
         )
 
-        login_manager.login_view = "auth.login"
+        login_manager.login_view = ".login"
 
         @login_manager.user_loader
         def load_user(user_id):
@@ -45,5 +54,26 @@ class zAuth:
             user = db.session.execute(stmt).scalar_one_or_none()
             app.logger.debug(f'Loaded user {user}')
             return user
+        
+        @app.context_processor
+        def inject_navigation():
+
+            login_uri = url_for('auth.login')
+            logout_uri = url_for('auth.logout')
+            # dashboard_uri = url_for('dashboard')
+            dashboard_uri = url_for('auth.protected')
+
+            if current_user.is_authenticated:
+                links = [
+                    ('Home', url_for('index')),
+                    ('Dashboard', dashboard_uri),
+                    ('Log Out', logout_uri),
+                ]
+            else:
+                links = {
+                    ('Home', url_for('index')),
+                    ('Log In', login_uri),
+                }
+            return {'navigation': links}
 
         app.register_blueprint(auth_bp, url_prefix='/auth')
